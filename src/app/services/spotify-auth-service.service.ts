143e9a9157f69b2d0d27370d2e689ad7710e9f30
paulmcgonigle67 <s00210326@atu.ie>
2023-04-07 21:43:31 +0200

    change to authservice

--- src/app/services/spotify-auth-service.service.ts
+++ src/app/services/spotify-auth-service.service.ts
@@ -13,10 +13,16 @@ export class SpotifyAuthServiceService {
  
 //o redirect the user to the Spotify Authorization Endpoint, you can use the window.location object in JavaScript to redirect the user to the URL.
   redirectToSpotifyAuth(){
-    const clientId = `${environment.clientId}&`;
-    const redirectUri = `${environment.redirectUrl}&`;
-    const scope = `${environment.scopes}&`;
-    // const responseType = 'response_type=token&show_dialog=true';
+    // const clientId = `${environment.clientId}&`;
+    // const redirectUri = `${environment.redirectUrl}&`;
+    // const scope = `${environment.scopes}&`;
+    // // const responseType = 'response_type=token&show_dialog=true';
+    // const responseType = 'token';
+
+    // const spotifyAuthUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=${responseType}`;
+    const clientId = environment.clientId;
+    const redirectUri = environment.redirectUrl;
+    const scope = environment.scopes.join(' ');
     const responseType = 'token';
 
     const spotifyAuthUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=${responseType}`;
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,17 +1,16 @@
 export const environment = {
     production: true,
-    
     clientId: 'c4cd5531ce004764a2ef3a24bcb776b4',
     authEndpoint: 'https://accounts.spotify.com/authorize',
     redirectUrl: 'https://moodchartapp-production.up.railway.app/login',
-    scopes:[
-        "user-read-currently-playing", 
-    "user-read-recently-played", 
-    "user-read-playback-state", 
-    "user-top-read", 
-    "user-modify-playback-state", 
-    "user-library-read", 
-    "playlist-read-private", 
-    "playlist-read-collaborative" 
+    scopes: [
+      "user-read-currently-playing", 
+      "user-read-recently-played", 
+      "user-read-playback-state", 
+      "user-top-read", 
+      "user-modify-playback-state", 
+      "user-library-read", 
+      "playlist-read-private", 
+      "playlist-read-collaborative" 
     ]
   };
