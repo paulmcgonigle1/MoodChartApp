1ee040104f03ab2e42d5a212d66f40f07a01a4d1
paulmcgonigle67 <s00210326@atu.ie>
2023-04-08 11:28:34 +0200

    fixing ampersand

--- src/app/services/spotify-auth-service.service.ts
+++ src/app/services/spotify-auth-service.service.ts
@@ -13,9 +13,10 @@ export class SpotifyAuthServiceService {
  
 //o redirect the user to the Spotify Authorization Endpoint, you can use the window.location object in JavaScript to redirect the user to the URL.
   redirectToSpotifyAuth(){
-    const clientId = `${environment.clientId}&`;
-    const redirectUri = `${environment.redirectUrl}&`;
-    const scope = `${environment.scopes}&`;
+    const clientId = `${environment.clientId}`;
+    const redirectUri = `${environment.redirectUrl}`;
+    const scope = `${environment.scopes}`;
+
     // const responseType = 'response_type=token&show_dialog=true';
     const responseType = 'token';
 
