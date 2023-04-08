75ab474605be4bed88042fb0b1479961c2bfdd21
paulmcgonigle67 <s00210326@atu.ie>
2023-04-08 22:00:17 +0200

    one last try

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,15 @@
 export const environment = {
   production: true,
   clientId: 'c4cd5531ce004764a2ef3a24bcb776b4',
-  redirectUrl: 'https://moodchartapp-production-b8d2.up.railway.app/login',
+  redirectUrl: 'http://moodchartapp-production-b8d2.up.railway.app/login/',
   scopes:[
-    "user-read-currently-playing", 
-    "user-read-recently-played", 
-    "user-read-playback-state", 
-    "user-top-read", 
-    "user-modify-playback-state", 
-    "user-library-read", 
-    "playlist-read-private", 
-    "playlist-read-collaborative" 
+      "user-read-currently-playing", 
+  "user-read-recently-played", 
+  "user-read-playback-state", 
+  "user-top-read", 
+  "user-modify-playback-state", 
+  "user-library-read", 
+  "playlist-read-private", 
+  "playlist-read-collaborative" 
   ]
 };
