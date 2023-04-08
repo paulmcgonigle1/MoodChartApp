919fa9dd67732c1d9f40a06f263a4fe2cc6003a4
paulmcgonigle67 <s00210326@atu.ie>
2023-04-08 11:09:02 +0200

    change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   clientId: 'c4cd5531ce004764a2ef3a24bcb776b4',
-  redirectUrl: 'https://moodchartapp-production.up.railway.app/login/',
+  redirectUrl: 'https://moodchartapp-production-b8d2.up.railway.app/login/',
   scopes:[
       "user-read-currently-playing", 
   "user-read-recently-played", 
