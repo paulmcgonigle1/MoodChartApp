597aebc6e28c75cdece64084cf517ecdfe52fdeb
paulmcgonigle67 <s00210326@atu.ie>
2023-04-09 09:48:58 +0200

    updated firebase redirect

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   clientId: 'c81078cdef9b4b0a9b67759341b0e85f',
-  redirectUrl: 'https://moodify4-c62cf.web.app/login',
+  redirectUrl: 'https://moodify5.web.app/login',
   scopes:[
       "user-read-currently-playing", 
   "user-read-recently-played", 
